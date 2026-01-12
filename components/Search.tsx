4fa607617a153687ef3e6f4217541e2e5ad64f73
Riflan Mohamed <rizlanahmd4545@gmail.com>
2026-01-12 15:05:14 +0530

    'saerch part completed'

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import icons from '@/constants/icons';
+import { router, useLocalSearchParams, usePathname } from 'expo-router';
+import React, { useState } from 'react';
+import { Image, TextInput, TouchableOpacity, View } from 'react-native';
+import { useDebouncedCallback } from 'use-debounce';
 
 const Search = () => {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
+
+    const path = usePathname();
+    const params = useLocalSearchParams<{ query?: string }>();
+
+    const [search, setSearch] = useState(params.query);
+
+    const debounceSearch = useDebouncedCallback(
+        (text: string) => {
+            router.setParams({ query: text })
+        }, 500
+    );
+
+
+    const handleSearch = (text: string) => {
+        setSearch(text)
+        debounceSearch(text)
+    }
+    return (
+        <View className='flex flex-row items-center mx-3 justify-between w-[95%] px-5 rounded-lg bg-gray-100 border border-gray-100 mt-5 py-2'>
+            <View className='flex-1 flex- flex-row items-center justify-start z-50'>
+                <Image source={icons.search} className='size-5' />
+                <TextInput value={search}
+                    onChangeText={handleSearch}
+                    placeholder='Search For Anything                       '
+                    className='text-sm font-rubik text-black ml-2 text-1'
+                />
+
+            </View>
+            <TouchableOpacity>
+                <Image source={icons.filter} className='size-5' />
+            </TouchableOpacity>
+
+
+        </View>
+    )
 }
 
 export default Search
